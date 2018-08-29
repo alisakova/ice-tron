4bb71c05994989f719d3facc33c4696e63ee73f5
alisakova <coctail_rossini@mail.ru>
2018-08-29 12:32:34 +0300

    fix script

--- src/js/address.js
+++ src/js/address.js
@@ -1,9 +1,27 @@
 var addressList = document.querySelectorAll(".service-list__city-name");
 var body = document.querySelector("body");
 
-if (addressList) {
-  var openServiceAddressInfo = function(elem) {
-    elem.addEventListener("click", function(e) {
+$(document).ready(function(){
+  if (addressList) {
+    var openServiceAddressInfo = function(elem) {
+      elem.addEventListener("click", function(e) {
+        e.stopPropagation();
+        var addressInfoBlockActive = document.querySelectorAll(
+          ".service-address.active"
+        );
+        addressInfoBlockActive.forEach(function(item) {
+          item.classList.remove("active");
+        });
+        var addressInfoBlock = elem.querySelector(".service-address");
+        if (addressInfoBlock) {
+          addressInfoBlock.classList.add("active");
+        }
+      });
+    };
+
+    addressList.forEach(openServiceAddressInfo);
+
+    body.addEventListener("click", function(e) {
       e.stopPropagation();
       var addressInfoBlockActive = document.querySelectorAll(
         ".service-address.active"
@@ -11,22 +29,6 @@ if (addressList) {
       addressInfoBlockActive.forEach(function(item) {
         item.classList.remove("active");
       });
-      var addressInfoBlock = elem.querySelector(".service-address");
-      if (addressInfoBlock) {
-        addressInfoBlock.classList.add("active");
-      }
     });
-  };
-
-  addressList.forEach(openServiceAddressInfo);
-
-  body.addEventListener("click", function(e) {
-    e.stopPropagation();
-    var addressInfoBlockActive = document.querySelectorAll(
-      ".service-address.active"
-    );
-    addressInfoBlockActive.forEach(function(item) {
-      item.classList.remove("active");
-    });
-  });
-}
+  }
+});
